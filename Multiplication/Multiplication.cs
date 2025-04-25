0f891cf7a938e44621d2319fdff95c9250bc74fe
payal-suthar31 <piyusuthar.official@gmail.com>
2025-04-25 16:39:59 +0530

    Multiplication Project added

---
+++ Multiplication/Multiplication.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("enter your number:");
+        int number = Convert.ToInt32(Console.ReadLine());
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{number}*{i}={number * i}");
+        }
+    }
+}
+
