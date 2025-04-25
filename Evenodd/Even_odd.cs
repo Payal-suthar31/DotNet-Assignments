b0231b3f2e348b8b48bc2abb615b3e3fe6ea2082
payal-suthar31 <piyusuthar.official@gmail.com>
2025-04-25 16:37:52 +0530

    evenodd project initial commit

---
+++ Evenodd/Even_odd.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("enter your number:");
+        int number = Convert.ToInt32(Console.ReadLine());
+        {
+            if (number % 2 == 0)
+                Console.WriteLine($"Even number is {number}");
+
+            else
+                Console.WriteLine($"Odd number is {number}");
+        }
+
+    }
+}
